ce5eef96c30ff83e4ea7443b135102168131f69e
PANJIBOTz <dekpanji23@gmail.com>
2021-12-11 21:42:51 +0800

    SelfBot01

---
+++ message/info1.js
@@ -0,0 +1,15 @@
+exports.bot = (gender,fake,nomerOwner)=> {
+return`
+*╭─▸*「 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧𝗭 」
+*│*
+*│▸* Nama : ${fake}
+*│▸* Gender : ${gender}
+*│▸* Bio : Hai, pacar elaina memakai WhatsApp.
+*│▸* Owner : ${nomerOwner}
+*│▸* Tgl pembuatan : 25-07-2021
+*│▸* Tgl release : 29-09-2021
+*│▸* Image : Gawr Gura
+*│▸* Voice : Nina kawai
+*│*
+*╰────────────[*`
+}
